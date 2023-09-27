6dcaeb700a41a2f290c92f04defb7620294b7155
Iván <ivanmanu2004@gmail.com>
2023-09-27 16:03:08 +0200

    Deporte, lugar y red social favorita

--- Prueba.java
+++ Prueba.java
@@ -4,6 +4,7 @@ public class Prueba{
 
 	System.out.println("Deporte favorito natación");
 	System.out.println("Mi lugar favorito es la playa");
+	System.out.println("Mi red social favorita es instagram");
 
 	}
 
